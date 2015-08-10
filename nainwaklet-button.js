5aae91e674bf245ea127847503caa55d190ebee0
Sylvain Prat <sylvain.prat@gmail.com>
2015-08-10 21:53:32 +0200

    Don't keep adding script tags in the document

--- nainwaklet-button.js
+++ nainwaklet-button.js
@@ -11,10 +11,13 @@
 
     function initNainwakletButton(id, nainwakletPath) {
         var button = document.getElementById(id),
-            href = "javascript:(function(){var%20s=document.createElement('script');s.setAttribute('src','"
-                    + baseUrl
-                    + (nainwakletPath || "nainwaklet.js")
-                    + "');document.getElementsByTagName('head')[0].appendChild(s);})();";
+            scriptUrl = baseUrl + (nainwakletPath || "nainwaklet.js"),
+            href = "javascript:(function(){var%20d=document,%20b=d.body,%20s=d.getElementById('nainwaklet_js');"
+                    + "if%20(s)%20b.removeChild(s);"
+                    + "s=d.createElement('script');"
+                    + "s.setAttribute('id','nainwaklet_js');"
+                    + "s.setAttribute('src','" + scriptUrl + "');"
+                    + "b.appendChild(s);}());";
         button.setAttribute("href", href);
     }
 
