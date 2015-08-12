f63902b4c312383c61ed4bfe33bcd102850a7762
Sylvain Prat <sylvain.prat@gmail.com>
2015-08-12 09:07:43 +0200

    Use classes

--- nainwaklet.js
+++ nainwaklet.js
@@ -1,7 +1,11 @@
 /*global
     window, document, alert, XMLHttpRequest
  */
-(function (exports) {
+/*jslint
+    this: true
+ */
+
+(function () {
     "use strict";
 
     function log(msg) {
@@ -10,117 +14,33 @@
         }
     }
 
+    function extend(target, source) {
+        var names = Object.keys(source);
+        names.forEach(function (name) {
+            target[name] = source[name];
+        });
+    }
+
+    function defineClass(members) {
+        // create a constructor function
+        function constructor() {
+            if (this.initialize) {
+                this.initialize.apply(this, arguments);
+            }
+        }
+
+        // add the members to the prototype
+        extend(constructor.prototype, members);
+
+        // return the constructor
+        return constructor;
+    }
+
     function startsWith(str, substr) {
         return str.slice(0, substr.length) === substr;
     }
 
-    function asyncGet(url, onSuccess, onFailure) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', encodeURI(url), true);  // asynchronous
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {  // response received and loaded
-                if (xhr.status === 200) {
-                    onSuccess(xhr.responseText, xhr.statusText);
-                } else {
-                    onFailure(xhr.statusText);
-                }
-            }
-        };
-        xhr.send(null);
-    }
-
-    function gameUrl(name, IDS, suffix) {
-        //http://www.nainwak.com/accueil/resume.php?IDS=b363601260a8be5d86d1034efba99568&errmsg=
-        return 'http://www.nainwak.com/jeu/' + name + '.php?IDS=' + IDS + (suffix || "");
-    }
-
-    function sendUpdate(IDS) {
-        var pagesUrls = {
-            //menu: gameUrl('menu', IDS),  // Menu principal
-            pager: gameUrl('pager', IDS),  // Pager : PV/PA, compteur des messages
-            //pub: gameUrl('pub', IDS),  // Publicités
-            //map: gameUrl('map', IDS),  // Carte de la détection
-            detect: gameUrl('detect', IDS),  // Détection
-            //deplac: gameUrl('deplac', IDS),  // Action
-            invent: gameUrl('invent', IDS),  // Inventaire
-            perso: gameUrl('perso', IDS),  // Fiche de perso
-            even: gameUrl('even', IDS, "&duree=240&type=ALL"),  // événements : tous types d'evts sur 10 jours
-            //chat: gameUrl('chat', IDS),  // Messagerie
-            //guilde: gameUrl('guilde', IDS),  // Guilde
-            encyclo: gameUrl('encyclo', IDS)  // Encyclopédie
-        };
-        Object.keys(pagesUrls).forEach(function (name) {
-            var url = pagesUrls[name];
-            log(name + ": " + url);
-        });
-        //asyncGet(pagesUrls.detect, log, log);
-    }
-
-    function createUpdateButton() {
-        var button = document.createElement("button");
-        button.setAttribute("type", "button");
-        button.innerHTML = "Lancer MAJ";
-        button.style.marginBottom = "10px";
-        button.onclick = function () {
-            var IDS = getIDS();
-            if (IDS) {
-                sendUpdate(IDS);
-            } else {
-                alert("Erreur : ID de session non trouvé, vous devez vous connecter d'abord");
-            }
-        };
-        return button;
-    }
-
-    function createHubMenu(hubPath) {
-        var iframe = document.createElement("iframe");
-        iframe.setAttribute("src", getBaseUrl() + hubPath);
-        iframe.style.width = "100%";
-        return iframe;
-    }
-
-    function createHubUI(hubPath) {
-        var ui = document.createElement("div"),
-            updateButton = createUpdateButton(),
-            hubMenu = createHubMenu(hubPath);
-        ui.setAttribute("id", "hubUI");
-        ui.style.textAlign = "center";
-        ui.appendChild(updateButton);
-        ui.appendChild(hubMenu);
-        return ui;
-    }
-
-    function infoFrameLoaded() {
-        var infoDoc = window.info.document,
-            url = infoDoc.location,
-            html = infoDoc.documentElement.innerHTML;
-        log("info frame loaded: " + url);
-    }
-
-    function toggleHubUI(hubPath, storage) {
-        var infoFrame = window.info.frameElement,
-            pubDoc = window.pub.document,
-            pubBody = pubDoc.body,
-            hubUI = pubDoc.getElementById("hubUI");
-
-        if (!hubUI) {
-            // backup the existing content
-            storage.pubBodyInnerHTML = pubBody.innerHTML;
-            storage.infoFrameLoaded = infoFrameLoaded;
-
-            // replace the existing content by the hub UI
-            hubUI = createHubUI(hubPath);
-            pubBody.innerHTML = "";
-            pubBody.appendChild(hubUI);
-            infoFrame.addEventListener('load', infoFrameLoaded, false);
-        } else {
-            // restore the initial content
-            pubBody.innerHTML = storage.pubBodyInnerHTML;
-            infoFrame.removeEventListener('load', storage.infoFrameLoaded, false);
-        }
-    }
-
-    function getBaseUrl() {
+    function getScriptBaseUrl() {
         var scriptUrl = document.scripts[document.scripts.length - 1].src;
         return scriptUrl.substring(0, scriptUrl.lastIndexOf('/') + 1);
     }
@@ -142,16 +62,168 @@
         return getQueryParams().IDS;
     }
 
-    function runOnNainwak(hubPath) {
-        if (startsWith(window.location.href, "http://www.nainwak.com/jeu/index.php")) {
-            // initialize the plugin storage
-            exports.nainwaklet = exports.nainwaklet || {};
-            // toggle the UI
-            toggleHubUI(hubPath, exports.nainwaklet);
-        } else {
-            alert("Erreur : ce bouton fonctionne uniquement sur la page jeu de Nainwak");
+    /*
+    function asyncGet(url, onSuccess, onFailure) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', encodeURI(url), true);  // asynchronous
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {  // response received and loaded
+                if (xhr.status === 200) {
+                    onSuccess(xhr.responseText, xhr.statusText);
+                } else {
+                    onFailure(xhr.statusText);
+                }
+            }
+        };
+        xhr.send(null);
+    }
+    */
+
+    /* Spy class */
+    var Spy = defineClass({
+        //infoFrame = window.info.frameElement,
+        //this.infoFrameLoaded = infoFrameLoaded;
+        // replace the existing content by the hub UI
+        //infoFrame.addEventListener('load', infoFrameLoaded, false);
+        //infoFrame.removeEventListener('load', storage.infoFrameLoaded, false);
+        createUI: function () {
+            var button = document.createElement("button"),
+                that = this;
+            button.setAttribute("class", "spy");
+            button.setAttribute("type", "button");
+            button.innerHTML = "Lancer MAJ";
+            // TODO: move in CSS
+            button.style.marginBottom = "10px";
+            button.onclick = function () {
+                var IDS = getIDS();
+                if (IDS) {
+                    that.sendUpdate(IDS);
+                } else {
+                    alert("Erreur : ID de session non trouvé, vous devez vous connecter d'abord");
+                }
+            };
+            return button;
+        },
+        /*
+        infoFrameLoaded_: function () {
+            var infoDoc = window.info.document,
+                url = infoDoc.location,
+                html = infoDoc.documentElement.innerHTML;
+            log("info frame loaded: " + url);
+        },
+        */
+        gameUrl: function (name, IDS, suffix) {
+            //http://www.nainwak.com/accueil/resume.php?IDS=b363601260a8be5d86d1034efba99568&errmsg=
+            return 'http://www.nainwak.com/jeu/' + name + '.php?IDS=' + IDS + (suffix || "");
+        },
+        sendUpdate: function (IDS) {
+            var pagesUrls = {
+                //menu: this.gameUrl('menu', IDS),  // Menu principal
+                pager: this.gameUrl('pager', IDS),  // Pager : PV/PA, compteur des messages
+                //pub: this.gameUrl('pub', IDS),  // Publicités
+                //map: this.gameUrl('map', IDS),  // Carte de la détection
+                detect: this.gameUrl('detect', IDS),  // Détection
+                //deplac: this.gameUrl('deplac', IDS),  // Action
+                invent: this.gameUrl('invent', IDS),  // Inventaire
+                perso: this.gameUrl('perso', IDS),  // Fiche de perso
+                even: this.gameUrl('even', IDS, "&duree=240&type=ALL"),  // événements : tous types d'evts sur 10 jours
+                //chat: this.gameUrl('chat', IDS),  // Messagerie
+                //guilde: this.gameUrl('guilde', IDS),  // Guilde
+                encyclo: this.gameUrl('encyclo', IDS)  // Encyclopédie
+            };
+            Object.keys(pagesUrls).forEach(function (name) {
+                var url = pagesUrls[name];
+                log(name + ": " + url);
+            });
+            //asyncGet(pagesUrls.detect, log, log);
         }
+    });
+
+    /* Hub class */
+    var Hub = defineClass({
+        initialize: function (url) {
+            this.url = url;
+        },
+        createUI: function () {
+            var iframe = document.createElement("iframe");
+            iframe.setAttribute("class", "hub");
+            iframe.setAttribute("src", this.url);
+            // TODO: move in CSS
+            iframe.style.width = "100%";
+            return iframe;
+        }
+    });
+
+    /* Manager class */
+    var Manager = defineClass({
+        initialize: function (hubUrl) {
+            this.spy = new Spy();
+            this.hub = new Hub(hubUrl);
+            this.enabled = false;
+            this.container = window.pub.document.body;  // container for the UI
+        },
+        enable: function () {
+            if (this.enabled) {
+                return;
+            }
+
+            var ui = this.createUI();
+
+            // backup the initial content
+            this.containerInitialContent = this.container.innerHTML;
+
+            // replace by our UI
+            this.container.innerHTML = "";
+            this.container.appendChild(ui);
+
+            this.enabled = true;
+        },
+        disable: function () {
+            if (!this.enabled) {
+                return;
+            }
+
+            // restore the initial content
+            this.container.innerHTML = this.containerInitialContent;
+
+            this.enabled = false;
+        },
+        createUI: function () {
+            var container = document.createElement("div"),
+                spyUI = this.spy.createUI(),
+                hubUI = this.hub.createUI();
+
+            container.setAttribute("class", "nainwaklet");
+
+            // TODO: move in CSS
+            container.style.textAlign = "center";
+
+            if (spyUI) {
+                container.appendChild(spyUI);
+            }
+
+            if (hubUI) {
+                container.appendChild(hubUI);
+            }
+
+            return container;
+        }
+    });
+
+    // put here the path to the hub page, relatively to the nainwaklet path
+    // TODO: put it in the nainwaklet button script tag instead
+    var hubUrl = getScriptBaseUrl() + "hub.html";
+
+    if (startsWith(window.location.href, "http://www.nainwak.com/jeu/index.php")) {
+        if (!window.nainwaklet) {  // not initialized => enable
+            window.nainwaklet = new Manager(hubUrl);
+            window.nainwaklet.enable();
+        } else {  // already initialized => disable
+            window.nainwaklet.disable();
+            delete window.nainwaklet;
+        }
+    } else {
+        alert("Erreur : ce bouton fonctionne uniquement sur la page jeu de Nainwak");
     }
 
-    runOnNainwak("hub.html");
-}(window));
+}());
