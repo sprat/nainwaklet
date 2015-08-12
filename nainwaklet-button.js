391ac8a60e78011b35db02ba9a05a60e229501f3
Sylvain Prat <sylvain.prat@gmail.com>
2015-08-12 22:17:07 +0200

    Simplified the nainwaklet button code... to prepare a refactoring

--- nainwaklet-button.js
+++ nainwaklet-button.js
@@ -6,18 +6,17 @@
 
     var scriptUrl = document.scripts[document.scripts.length - 1].src,
         baseUrl = scriptUrl.substring(0, scriptUrl.lastIndexOf('/') + 1),
-        defaultHubUrl = baseUrl + "hub.html",
-        defaultNainwakletUrl = baseUrl + "nainwaklet.js";
+        nainwakletJs = baseUrl + "nainwaklet.js",
+        defaultHubUrl = baseUrl + "hub.html";
 
-    function initNainwakletButton(id, hubUrl, nainwakletUrl) {
+    function initNainwakletButton(id, hubUrl) {
         var button = document.getElementById(id),
-            src = (nainwakletUrl || defaultNainwakletUrl),
-            hub = (hubUrl || defaultHubUrl),
-            href = "javascript:(function(){var%20d=document,%20b=d.body,%20s=d.getElementById('nainwaklet_js');"
+            hub = hubUrl || defaultHubUrl,
+            href = "javascript:(function(){var%20d=document,b=d.body,s=d.getElementById('nainwaklet_js');"
                     + "if%20(s)%20b.removeChild(s);"
                     + "s=d.createElement('script');"
                     + "s.setAttribute('id','nainwaklet_js');"
-                    + "s.setAttribute('src','" + src + "');"
+                    + "s.setAttribute('src','" + nainwakletJs + "');"
                     + "s.setAttribute('data-hub','" + hub + "');"
                     + "b.appendChild(s);}());";
         button.setAttribute("href", href);
