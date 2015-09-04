d6d5af623a59bf3960edcec400c7c5ac4bc82dbc
Sylvain Prat <sylvain.prat@gmail.com>
2015-09-04 17:26:01 +0200

    Added a fixture and a failing test

--- tests.js
+++ tests.js
@@ -1,10 +1,23 @@
 /*global
-    QUnit
+    QUnit, $, Nainwaklet
  */
 
 (function () {
     'use strict';
 
+    function loadFixture(assert, url, processResponse) {
+        var done = assert.async(),
+            fail = function () {
+                assert.ok(false, 'Fixture not found: ' + url);
+            };
+
+        $.ajax({url: url})
+            .done(processResponse)
+            .fail(fail)
+            .always(done);
+    }
+
+    /* Nainwaklet tests */
     QUnit.module('Nainwaklet');
 
     QUnit.test('pages', function (assert) {
@@ -13,5 +26,16 @@
 
     QUnit.test('createApplication', function (assert) {
         assert.ok(Nainwaklet.createApplication, 'Nainwaklet should have an "createApplication" function');
+    });
+
+    QUnit.test('detect', function (assert) {
+        var detect = Nainwaklet.pages.get('detect');
+
+        loadFixture(assert, 'fixtures/detect.html', function (response) {
+            var info = detect.analyze(response);
+            assert.strictEqual(info.localization.position[0] , 13, 'X-position is valid');
+            assert.strictEqual(info.localization.position[1] , 5, 'Y-position is valid');
+            assert.strictEqual(info.localization.world , 'Ronain Graou', 'World is valid');
+        });
     });
 }());
