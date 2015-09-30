e026e0f781b88976c1ee448ad0de730bfae7504a
Sylvain Prat <sylvain.prat@gmail.com>
2015-09-30 23:00:10 +0200

    We now have a dev server running on node, like the future tools

--- run_server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd "%~dp0"
-start "Web Server" python.exe -m SimpleHTTPServer 8080
-popd
--- run_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pushd $(dirname $0) > /dev/null
-python2 -m SimpleHTTPServer 8080
-popd > /dev/null
