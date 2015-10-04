5bd3014685cb195e858209f458a748a928f16d3f
Sylvain Prat <sylvain.prat@gmail.com>
2015-10-04 12:28:44 +0200

    Added a test of the isInGame function + improved some tests

--- test/nainwak.js
+++ test/nainwak.js
@@ -8,10 +8,30 @@ define(['app/nainwak'], function (nainwak) {
         assert.strictEqual(nainwak.gameUrl('detect'), 'http://www.nainwak.com/jeu/detect.php', 'Detect URL');
     });
 
+    QUnit.test('imageUrl', function (assert) {
+        assert.strictEqual(nainwak.imageUrl('avatar/perso/test.png'), 'http://www.nainwak.com/images/avatar/perso/test.png', 'Image URL');
+    });
+
+    QUnit.test('isInGame', function (assert) {
+        var window = {},
+            link = document.createElement('a');
+
+        // Note: the link will mimick the location API
+        window.location = link;
+
+        link.href = nainwak.gameUrl();
+        assert.ok(nainwak.isInGame(window), 'Game window');
+
+        link.href = 'http://www.google.com';
+        assert.notOk(nainwak.isInGame(window), 'Non-game window');
+    });
+
+    // TODO: test getUser
+
     QUnit.test('pages.list', function (assert) {
         var pages = nainwak.pages;
         assert.ok(pages.list.length > 0, 'Non-empty list');
-        assert.strictEqual(pages.list[0], pages.byName('detect'), 'First element is the detect page');
+        assert.strictEqual(pages.list[0].name, 'detect', 'First element is the detect page');
     });
 
     QUnit.test('pages.byName', function (assert) {
