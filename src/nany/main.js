ee2993b52a144a05184d4cc6d2f367f80a951cb4
Sylvain Prat <sylvain.prat@gmail.com>
2015-10-08 14:07:43 +0200

    Removed the unnecessary runOnNainwak module

--- src/nany/main.js
+++ src/nany/main.js
@@ -1,5 +1,5 @@
 /* Module API */
-define(['./user', './application', './bookmarklets', './runOnNainwak'], function (User, Application, bookmarklets, runOnNainwak) {
+define(['./user', './application', './bookmarklets', './nainwak', './settings', 'utils/css'], function (User, Application, bookmarklets, nainwak, settings, css) {
     'use strict';
 
     var module = {
@@ -8,7 +8,29 @@ define(['./user', './application', './bookmarklets', './runOnNainwak'], function
         Application: Application
     };
 
-    runOnNainwak(module);
+    // create an Application running on the Nainwak game
+    function runOnNainwak() {
+        if (!nainwak.isInGame(window)) {
+            return;
+        }
+
+        var pubDoc = window.frames.pub.document,
+            infoFrame = window.frames.info.frameElement,
+            nain = nainwak.getNain(window);
+
+        // insert the CSS file if needed (we never remove it!)
+        css.insertLink(settings.cssUrl, pubDoc);
+
+        // create the Hub and assign it to the external api
+        module.app = Application({
+            user: User(nain.nom, nain.image),
+            channel: settings.channel,
+            container: pubDoc.body,
+            infoFrame: infoFrame
+        });
+    }
+
+    runOnNainwak();
 
     return module;
 });
