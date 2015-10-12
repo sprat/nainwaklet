415ea3f5fac03b5a55ff9d99660a95f1353c1af3
Sylvain Prat <sylvain.prat@gmail.com>
2015-10-13 00:09:24 +0200

    One more thing to do

--- js/tests/nainwak.js
+++ js/tests/nainwak.js
@@ -43,7 +43,9 @@ define(['nany/nainwak'], function (nainwak) {
 
     QUnit.test('pages.byUrl', function (assert) {
         var pages = nainwak.pages;
-        assert.strictEqual(pages.byUrl('http://www.nainwak.com/jeu/detect.php').name, 'detect', 'Detect page by URL');
-        assert.strictEqual(pages.byUrl('http://www.nainwak.com/jeu/invalid.php'), undefined, 'Invalid URL');
+        assert.strictEqual(pages.byUrl('http://www.nainwak.com/jeu/detect.php').name, 'detect', 'Detect page');
+        //TODO: we want the following test to pass
+        //assert.strictEqual(pages.byUrl('http://www.nainwak.com/jeu/even.php?duree=240&typeALL').name, 'even', 'Event page with query string');
+        assert.strictEqual(pages.byUrl('http://www.nainwak.com/jeu/invalid.php'), undefined, 'Invalid page');
     });
 });
