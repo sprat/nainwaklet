2bd48528664983b291a8f8af187b008447934ba4
Sylvain Prat <sylvain.prat@gmail.com>
2015-10-13 13:29:11 +0200

    Same example with pubnub

--- js/nany/connection.js
+++ js/nany/connection.js
@@ -1,4 +1,29 @@
 define(['pusher', 'utils/base64', 'utils/log'], function (Pusher, base64, log) {
+    /*
+    function Connection(channelName) {
+        var pubnub = PUBNUB.init({
+                publish_key: 'pub-c-8be41a11-cbc5-4427-a5ad-e18cf5a466e4',
+                subscribe_key: 'sub-c-38ae8020-6d33-11e5-bf4b-0619f8945a4f'
+            }),
+            safeChannelName = base64.encodeUrl(channelName);
+
+        log('Connecting to ' + channelName);
+        log(safeChannelName);
+
+        function onMessage(data) {
+            log(data);
+        }
+
+        pubnub.subscribe({
+            channel: safeChannelName,
+            message: onMessage
+        });
+
+        return {
+        };
+    }
+    */
+
     function Connection(channelName) {
         var app_key = '408668d142e0e6fdab8f',
             pusher = new Pusher(app_key, {
