653f59d3c91aefd253428f1dcdb3fbf46ae137ef
Sylvain Prat <sylvain.prat@gmail.com>
2016-08-30 23:04:58 +0200

    Found a better way to start/stop the application in the bookmarklet

--- test/urls.js
+++ test/urls.js
@@ -11,13 +11,3 @@ test('urls.getImageUrl', function (assert) {
     assert.strictEqual(urls.getImageUrl('avatar/perso/test.png'), '/images/avatar/perso/test.png', 'Image URL');
     assert.end();
 });
-
-test('urls.isGameUrl', function (assert) {
-    assert.ok(urls.isGameUrl('http://www.nainwak.com/jeu/index.php'), 'Canonical game url');
-    assert.ok(urls.isGameUrl('http://nainwak.com/jeu/index.php'), 'No www game url');
-    assert.ok(urls.isGameUrl('https://www.nainwak.com/jeu/index.php'), 'Https game url');
-    assert.notOk(urls.isGameUrl('http://www.nainwak.com/'), 'Nainwak, but not game 1');
-    assert.notOk(urls.isGameUrl('http://www.nainwak.com/jeu/even.php'), 'Nainwak, but not game 2');
-    assert.notOk(urls.isGameUrl('http://www.google.fr'), 'External url');
-    assert.end();
-});
