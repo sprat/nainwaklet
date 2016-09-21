a59700c8ed7eaa6256c3cf36cb77d9626c6fcc79
Sylvain Prat <sylvain.prat@gmail.com>
2016-09-21 14:26:03 +0200

    Removed another source file that did not add much value

--- js/nany/index.js
+++ js/nany/index.js
@@ -1,6 +1,21 @@
+var Application = require('./application');
 var bookmarklets = require('./bookmarklets');
-var run = require('./run');
 var formules = require('./formules.json');
+
+//start/stop the Application on the Nainwak game page
+function run(config) {
+    var app = window.nanyApplication;
+
+    // if the application is already launched, stop it
+    if (app) {
+        app.destroy();
+        window.nanyApplication = undefined;  // can't call "delete" on window in some IE
+        return;
+    }
+
+    // start the application
+    window.nanyApplication = Application(config);
+}
 
 module.exports = Object.freeze({
     initializeBookmarklets: bookmarklets.initialize,
