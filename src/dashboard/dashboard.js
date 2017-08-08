0741144cbf1c3ddbed93d09747ce9e75fdc6b3dd
Sylvain Prat <sylvain.prat@gmail.com>
2017-08-08 17:56:47 +0200

    Removed some useless code

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -2,15 +2,7 @@ var styles = require('./dashboard.css');
 var contours = require('./contours.css');
 
 // TODO: remove the dashboard, should be the app's render function
-function Dashboard(applicationName, ring, storage, refreshUI) {
-    // refresh the UI when the authorization is changed in storage
-    // TODO: move this into the Ring class as it is related to that
-    storage.changed.add(function (key) {
-        if (key === 'authorization') {
-            refreshUI();
-        }
-    });
-
+function Dashboard(applicationName, ring) {
     function render(h) {
         return h('div', { class: styles.dashboard }, [
             h('div', { class: [contours.VNT, styles.dashboardTitle] }, applicationName),
