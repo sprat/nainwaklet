c9d021919f71bb464228bb796cf97d6fd355cdd0
Sylvain Prat <sylvain.prat@net-ng.com>
2017-08-23 18:13:24 +0200

    Better documentation of tricky things

--- test/fixtures/parse-html-document.js
+++ test/fixtures/parse-html-document.js
@@ -1,15 +1,18 @@
 function parseHTMLDocument(html) {
+    // The following implementation is not supported in some browsers
+    // (e.g. PhantomJS), so we use a more compatible implementation!
+    //var parser = new DOMParser();
+    //var doc = parser.parseFromString(html, 'text/html');
     var doc = document.implementation.createHTMLDocument('');
     doc.documentElement.innerHTML = html;
 
+    // set the baseURI so that the links are properly parsed (they should
+    // implement the Location API for some tests to pass)
     var base = doc.createElement('base');
     base.href = 'http://www.nainwak.com';
     doc.head.appendChild(base);
 
     return doc;
-    // The following implementation is not supported by PhantomJS:
-    //var parser = new DOMParser();
-    //return parser.parseFromString(html, 'text/html');
 }
 
 module.exports = parseHTMLDocument;
