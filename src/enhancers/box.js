fb6770e83c56703bd64f3c36f5b87f0aea133e9c
Sylvain Prat <sylvain.prat@net-ng.com>
2017-10-25 14:37:13 +0200

    Fixed a Box bug: it should not render if an empty array is returned by the render function.

--- src/enhancers/box.js
+++ src/enhancers/box.js
@@ -3,7 +3,7 @@ var styles = require('./box.css');
 function Box(content) {
     function render(h) {
         var rendered = h.render(content);
-        if (rendered) {
+        if (rendered && !Array.isArray(rendered) || rendered.length > 0) {
             return h('div', { class: styles.box }, rendered);
         }
     }
